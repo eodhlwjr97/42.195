917975682c3d769f6eb293a83eda14cd6692aae5
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2022-05-23 09:55:49 +0900

    Update BJ_10809_알파벳찾기.java

--- dummy/BJ_10809_알파벳찾기.java
+++ dummy/BJ_10809_알파벳찾기.java
@@ -29,6 +29,31 @@ public class BJ_10809_알파벳찾기 {
 			}
 			System.out.print(alphabet[0][i] + " ");
 		}
+		
+		
+		/** 다른사람 풀이 */
+		Scanner in = new Scanner(System.in);
+ 
+ 
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = -1;
+		}
+ 
+		String S = in.nextLine();
+ 
+		for(int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+    
+			if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 인 경우에만 초기화
+				arr[ch - 'a'] = i;
+			}
+		}
+ 
+		for(int val : arr) {	// 배열 출력
+			System.out.print(val + " ");
+		}
 
 	}
 
