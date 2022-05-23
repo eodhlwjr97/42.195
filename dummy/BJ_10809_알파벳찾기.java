bdf7ae48e09eee2880590c057bd2c978ff2d95a4
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2022-05-23 09:52:13 +0900

    Create BJ_10809_알파벳찾기.java

---
+++ dummy/BJ_10809_알파벳찾기.java
@@ -0,0 +1,35 @@
+package 문자열;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_10809_알파벳찾기 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+
+		// [0]단어순서, [1]나온 횟수
+		int[][] alphabet = new int[2][200]; // 26글자, 인덱스와 순서 맞추기
+		int index = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			int cNum = c;
+			index = i;
+			alphabet[1][cNum]++;
+			if(alphabet[1][cNum] >= 2) continue; // 처음 등장했을 때 1이므로 두번째 등장은 2부터 시작임
+			if (alphabet[0][cNum] == 0) { // 처음 등장하는 위치만 알고싶기때문
+				alphabet[0][cNum] = index; // a는 97부터 z 122까지 각 문자의 아스키코드값에 순서 담기
+			}
+		}
+
+		for (int i = 97; i <= 122; i++) {
+			if (alphabet[1][i] == 0) { // 어떤 알파벳이 단어에 포함되어 있지 않다면
+				alphabet[0][i] = -1;
+			}
+			System.out.print(alphabet[0][i] + " ");
+		}
+
+	}
+
+}
