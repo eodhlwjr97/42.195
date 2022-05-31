d13b280ff54ec756c5b8a2e4162e43875bb8ac29
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2022-06-01 00:33:50 +0900

    Create BJ_1550_16진수.java

---
+++ dummy/BJ_1550_16진수.java
@@ -0,0 +1,15 @@
+package 브론즈5;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_1550_16진수 {
+
+	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(System.in);
+		String x = in.next();
+		int y = Integer.parseInt(x, 16);		
+		System.out.println(y);
+	}
+
+}
