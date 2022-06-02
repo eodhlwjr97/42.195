4b5738914a666432afbb3876f34f85a92fb246f1
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2022-06-02 12:46:52 +0900

    Create BJ_5337_웰컴.java

---
+++ dummy/BJ_5337_웰컴.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    /* \ 을 앞에 붙여서 그 문자가 문자임을 표시. 단, \의 기능이 있기 때문에 \ 자체를 출력하고 싶은 경우엔 \\ */
+	public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
